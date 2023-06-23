76caacd5ce275467236bd8243b3f28b07698c6c0
datptgcc200072 <datptgcc200072@fpt.edu.vn>
2023-06-24 00:50:11 +0700

    c4
    Upload CategoriesController

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,180 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FPTBook.Models;
+using FPTBook.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FPTBook.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly FPTBookIdentityDbContext _context;
+
+        public CategoriesController(FPTBookIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Categories
+        [Authorize(Roles = "StoreOwner, Admin")]
+        public async Task<IActionResult> Index()
+        {
+              return _context.Category != null ? 
+                          View(await _context.Category.ToListAsync()) :
+                          Problem("Entity set 'FPTBookContext.Category'  is null.");
+        }
+
+        [Authorize(Roles = "StoreOwner, Admin")]
+        public async Task<IActionResult> RequestCategory()
+        {
+              return View(await _context.Category.ToListAsync());
+        }
+
+        // GET: Categories/Details/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Category == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Create
+        [Authorize(Roles = "StoreOwner, Admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Categories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "StoreOwner, Admin")]
+        public async Task<IActionResult> Create([Bind("Id,Name,Status")] Category category, string Name)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(category);
+                if(_context.Category.Where(a => a.Name == Name).ToList().Count != 0){
+                     return View(category);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(RequestCategory));
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Edit/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Category == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Status")] Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Category == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Category
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Category == null)
+            {
+                return Problem("Entity set 'FPTBookContext.Category'  is null.");
+            }
+            var category = await _context.Category.FindAsync(id);
+            if (category != null)
+            {
+                _context.Category.Remove(category);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+          return (_context.Category?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
