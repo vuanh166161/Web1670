0e12c1f5d22015e5f215a4a1a3992052d7336b4d
vuanh166161 <anhnvgcc200163@fpt.edu.vn>
2023-06-25 19:50:00 +0700

    C8 Publishers Controller

---
+++ Controllers/PublishersController.cs
@@ -0,0 +1,169 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FPTBook.Models;
+using FPTBook.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FPTBook.Controllers
+{
+     [Authorize(Roles = "StoreOwner, Admin")]
+    public class PublishersController : Controller
+    {
+        private readonly FPTBookIdentityDbContext _context;
+
+        public PublishersController(FPTBookIdentityDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Publishers
+        public async Task<IActionResult> Index()
+        {
+              return _context.Publisher != null ? 
+                          View(await _context.Publisher.ToListAsync()) :
+                          Problem("Entity set 'FPTBookContext.Publisher'  is null.");
+        }
+
+        // GET: Publishers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Publisher == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return View(publisher);
+        }
+
+        // GET: Publishers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Publishers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description")] Publisher publisher, string Name)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(publisher);
+                if(_context.Publisher.Where(a => a.Name == Name).ToList().Count != 0){
+                     return View(publisher);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publisher);
+        }
+
+        // GET: Publishers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Publisher == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher.FindAsync(id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            return View(publisher);
+        }
+
+        // POST: Publishers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description")] Publisher publisher)
+        {
+            if (id != publisher.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(publisher);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PublisherExists(publisher.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(publisher);
+        }
+
+        // GET: Publishers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Publisher == null)
+            {
+                return NotFound();
+            }
+
+            var publisher = await _context.Publisher
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+
+            return View(publisher);
+        }
+
+        // POST: Publishers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Publisher == null)
+            {
+                return Problem("Entity set 'FPTBookContext.Publisher'  is null.");
+            }
+            var publisher = await _context.Publisher.FindAsync(id);
+            if (publisher != null)
+            {
+                _context.Publisher.Remove(publisher);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PublisherExists(int id)
+        {
+          return (_context.Publisher?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
